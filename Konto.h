24ab2dbe4ead439a3d0a09326b2aceccccb5473f
cofi-to-go <filip.milenovic@outlook.de>
2024-11-04 20:16:06 +0100

    84-87

--- Konto.h
+++ Konto.h
@@ -3,25 +3,28 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <iomanip>
 
 
 class Transaktion
 {
 public:
     //Konstruktor
-    Transaktion(double betrag, const std::string &beschreibung);
+    Transaktion(float betrag, const std::string &beschreibung);
 
     //Destruktor
     ~Transaktion();
 
 
-    double get_betrag() const;
+    float get_betrag() const;
     std::string get_beschreibung() const;
-
+    std::string get_zeitstempel() const;
 
 private:
-    double m_betrag;
+    float m_betrag;
     std::string m_beschreibung;
+    std::time_t zeitstempel;
 };
 
 
@@ -37,12 +40,18 @@ private:
 
     static std::vector<Konto> alleKonten;
 
+    //logging
+    void log(const std::string &message) const;
+    static std::ostream* logger; //statisches logger objekt für alle konten
+
 public:
     Konto(const std::string &ki) : kontoinhaber(ki), kontonummer(naechsteKontonummer++) 
     {
         kontostand = 0.0;
         //speichert die aktuelle naechsteKontonummer in der Datei "kontonummer.dat"
         letzteKontonummer();
+        log("Konto erstellt mit Kontoinhaber: " + kontoinhaber + ", Startkontostand: " + std::to_string(kontostand));
+
     }
 
     Konto(const Konto& other , const std::string& kopieren) : kontoinhaber(other.kontoinhaber), kontonummer(naechsteKontonummer++), kontostand(0.0) {
@@ -83,13 +92,13 @@ public:
 
     static void exportiere_Konten_csv(const std::string &dateiname);
     static void lade_Konten_csv(const std::string& dateiname);
+    void exportiere_transaktionen_csv(const std::string &dateiname);
+    void lade_transaktionen_csv(const std::string &dateiname);
+    //static void import_Konten_csv();
 
-
-    
-
-    //static void zeige_Transaktionen();
-    void add_transaction(double betrag);
-    //4e transfer()
+    void drucke_kontoauszug() const;
+    void add_transaction(float betrag);
+    void transfer(Konto &zielkonto, float betrag, const std::string &grund);
 
 
 };
